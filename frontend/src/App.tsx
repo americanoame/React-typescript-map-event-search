80d8f0ab4cd22f823bfb152b8a3faeafe43babaa
Americano.Dev <andrenations@gmail.com>
2024-08-05 00:19:40 -0400

    Add routing and Navbar component to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
 
+type RoutePaths = {
+  home: string;
+};
 
-const App = () => {
+const routes: RoutePaths = {
+  home: '/',
+};
+
+const App: React.FC = () => {
   return (
-    <div>App</div>
-  )
-}
+    <Router>
+      <Navbar />
+      <div className="App">
+        <div className="main-content">
+          <Routes>
+            <Route path={routes.home} element={<div>Home</div>} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;

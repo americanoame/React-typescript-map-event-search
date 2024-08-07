5e66fe215a6357eae5d1cbb9c28d309c81b971cb
Americano.Dev <andrenations@gmail.com>
2024-08-07 08:10:22 -0400

    Add Home route to App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 type RoutePaths = {
   home: string;
@@ -16,7 +17,7 @@ const App: React.FC = () => {
       <div className="App">
         <div className="main-content">
           <Routes>
-            <Route path={routes.home} element={<div>Home</div>} />
+            <Route path={routes.home} element={<Home />} />
           </Routes>
         </div>
       </div>

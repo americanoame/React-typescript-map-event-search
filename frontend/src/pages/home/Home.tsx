40207d8edee8aa672b405395765f7c50ec05213c
Americano.Dev <andrenations@gmail.com>
2024-08-07 08:01:05 -0400

    Add new Home component and CSS styles
    - Add Home component with background image and overlay
    - Create main-container and background-image classes in home.css

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,18 @@
+import './home.css';
+
+const Home = () => {
+  return (
+    <div className="main-container">
+      <div className="background-image"></div>
+      <div className="overlay"></div>
+
+      <div className="main-content">
+        <div className="row">
+          <div className="col-md-4 col-lg-3 state-list"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
